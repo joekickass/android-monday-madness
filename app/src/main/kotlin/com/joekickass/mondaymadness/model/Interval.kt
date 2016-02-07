d9357d8a2bf11e5ebd32e08905b4e2881aa538c0
joekickass <tnilsson.x@gmail.com>
2016-02-07 20:20:09 +0100

    Fix for stupid Realm bug with Kotlin

--- app/src/main/kotlin/com/joekickass/mondaymadness/model/Interval.kt
+++ app/src/main/kotlin/com/joekickass/mondaymadness/model/Interval.kt
@@ -5,9 +5,9 @@ import io.realm.RealmObject
 /**
  * Simple domain object for Realm
  */
-class Interval : RealmObject() {
-    var workInMillis: Long = 0
-    var restInMillis: Long = 0
-    var repetitions = 1
-    var timestamp: Long = 0
+open class Interval : RealmObject() {
+    open var workInMillis: Long = 0
+    open var restInMillis: Long = 0
+    open var repetitions = 1
+    open var timestamp: Long = 0
 }
